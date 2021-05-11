16f5a065eb5f420e6ac2cac4b8a7ce8e44f43f94
sijabw <62145270+sijabw@users.noreply.github.com>
2021-05-11 18:35:27 +0300

    Repositories.Impl

---
+++ BaseRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.DAL.Repositories.Impl
+{
+    public abstract class BaseRepository<T>
+        : IRepository<T>
+        where T : class
+    {
+        private readonly DbSet<T> _set;
+        private readonly DbContext _context;
+
+        public BaseRepository(DbContext context)
+        {
+            _context = context;
+            _set = context.Set<T>();
+        }
+
+        public void Create(T item)
+        {
+            _set.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            var item = Get(id);
+            _set.Remove(item);
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(
+            Func<T, bool> predicate,
+            int pageNumber = 0,
+            int pageSize = 10)
+        {
+            return
+                _set.Where(predicate)
+                .Skip(pageSize * pageNumber)
+                .Take(pageNumber)
+                .ToList();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            return _set.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _set.ToList();
+        }
+
+        public IEnumerable<T> GetALL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        IEnumerable<T> IRepository<T>.Find(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IRepository<T>.Find(Func<T, bool> predicate, int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IRepository<T>.GetALL()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ SweetFabRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Catalog.DAL.EF;
+using Catalog.DAL.Entities;
+using Catalog.DAL.Repositories.Interfaces;
+
+namespace Catalog.DAL.Repositories.Impl
+{
+    public class SweetFabREpository
+        : BaseRepository<SweetFab>, ISweetFabRepository
+    {
+        internal SweetFabREpository(SweetFabContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
