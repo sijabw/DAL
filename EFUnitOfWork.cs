26447aaba1fc11c607e76c49a6a6d5e37aa47b6c
sijabw <62145270+sijabw@users.noreply.github.com>
2021-05-11 18:36:16 +0300

    UnitOfWork

---
+++ EFUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Catalog.DAL.Repositories.Interfaces;
+
+namespace Catalog.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ISweetFabRepository SweetFabs { get; }
+        ISweetRepository Sweets { get; }
+        void Save();
+    }
+}
