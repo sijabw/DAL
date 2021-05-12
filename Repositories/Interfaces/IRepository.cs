49006651308efe9ff3c1a6e2491364ce6c365dd8
sijabw <62145270+sijabw@users.noreply.github.com>
2021-05-12 07:16:47 +0300

    Repositories

---
+++ Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Catalog.DAL.Entities;
+
+namespace Catalog.DAL.Repositories.Interfaces
+{
+    public interface ISweetFabRepository
+            : IRepository<SweetFab>
+    {
+
+    }
+}
