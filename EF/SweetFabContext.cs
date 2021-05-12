c78e9aaea9ad08f469aef1e477c01ae564d0ec8e
sijabw <62145270+sijabw@users.noreply.github.com>
2021-05-12 07:16:01 +0300

    EF

---
+++ EF/SweetFabContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Catalog.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.DAL.EF
+{
+    public class SweetFabContext
+        : DbContext
+    {
+        public DbSet<SweetFab> Phones { get; set; }
+        public DbSet<Sweet> Orders { get; set; }
+
+        public SweetFabContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+
+}
