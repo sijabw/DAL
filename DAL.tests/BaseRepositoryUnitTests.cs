a570bfcdf36fee793852140e99fad773170a18d7
sijabw <62145270+sijabw@users.noreply.github.com>
2021-05-12 07:15:43 +0300

    DAL.tests

---
+++ DAL.tests/BaseRepositoryUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.DAL.Entities;
+using Catalog.DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+class TestSweetRepository
+    : BaseRepository < Sweet >
+{
+    public TestSweetRepository(DbContext context)
+        : base(context)
+    {
+    }
+}
