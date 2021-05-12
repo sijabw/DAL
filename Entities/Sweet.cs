fe8d1701825fe517961508528f23d6212080d756
sijabw <62145270+sijabw@users.noreply.github.com>
2021-05-12 07:16:22 +0300

    Entities

---
+++ Entities/Sweet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Catalog.DAL.Entities
+{
+    public class Sweet
+    {
+        public int SweetId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public object SweetFabID { get; set; }
+    }
+}
---
+++ Entities/SweetFab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Catalog.DAL.Entities
+{
+    public class SweetFab
+    {
+        public int SweetFabID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Director { get; set; }
+        public IEnumerable<Sweet> Streets { get; set; }
+        public IEnumerable<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public static object SweetFabID { get; set; }
+    }
+
+
+}
